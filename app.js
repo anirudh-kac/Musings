1955beb1f3d5e085223de52d249556328d814a89
anirudh-kac <anirudh.kac@yahoo.com>
2019-08-09 21:28:43 +0530

    modify post page

--- app.js
+++ app.js
@@ -6,15 +6,33 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.use(express.static("public"));
 
+//Array used for temporarily storing posts till database is integrated
+const post1={
+  text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.",
+  image:"image.jpeg",
+};
 
+const post2={
+  text:"thoughts thoushh hhd hha hha a mohfi iioadsyuf n shfiowf nmnosh h hsvhoishdv so isovhoishv osvhsiv ",
+  image:"image.jpeg",
+}
+const post4={
+  text:"Posts are arranged in a grid manner and the page is responsive",
+  image:"image.jpeg",
+}
 
+const post3={
+  text:"ayfiuu yfduywqf euyqwfu wufuwf w akagcua cacacuatuk gcuagtcu",
+  image:"image.jpeg",
+}
 
+const postsArray=[post1,post2,post3,post4];
 app.get("/",function(req,res){
   res.render("index");
 });
 
 app.get("/read",function(req,res){
-  res.render("read");
+  res.render("read",{posts:postsArray});
 });
 app.get("/post",function(req,res){
   res.render("post");
