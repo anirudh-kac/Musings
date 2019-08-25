ba19a5d924f0c4aadfecd38daf284df7e70631e9
Anirudh Kachroo <anirudh.kac@yahoo.com>
2019-08-25 18:50:28 +0530

    add Procfile, listen to correct port

--- app.js
+++ app.js
@@ -122,6 +122,6 @@ request(options,function(error,response,body){
 });
 
 
-app.listen(3000, function() {
+app.listen(3000  || process.env.PORT, function() {
   console.log("Server started on port 3000");
 });
